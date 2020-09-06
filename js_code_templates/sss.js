3ed969934cd6b1ec7076c61799fc4c88b54221c0
kymb0 <55473161+kymb0@users.noreply.github.com>
2020-09-07 09:07:24 +0930

    Update sss.js

--- js_code_templates/sss.js
+++ js_code_templates/sss.js
@@ -1,40 +1,25 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
 /*
+Complete the vowelsAndConsonants function in the editor below. It has one parameter, a string, , consisting of lowercase English alphabetic letters (i.e., a through z). The function must do the following:
+
+First, print each vowel in  on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in .
+Second, print each consonant (i.e., non-vowel) in  on a new line in the same order as it appeared in .
+
  * Complete the vowelsAndConsonants function.
  * Print your output using 'console.log()'.
  */
 function vowelsAndConsonants(s) {
     var i;
     var vowels = 'aeiou';
-
-    for (i = 0; i < s; i++)   {
-        if (s.includes(vowels))
-            console.log("dddd");
-        
+    var consonants = [];
+    for (i in s)   {
+        if (vowels.includes(s[i])){
+            console.log(s[i]);
         }
+    else {
+        consonants += s[i] + '\n';
+    }
+    }
+console.log(consonants);
 }
 
 Example above explained:
